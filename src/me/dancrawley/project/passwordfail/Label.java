ae723a27a0088bae203e17ce8897301521dd9fd4
Daniel Crawley <danielcrawley1998@gmail.com>
2015-06-12 11:20:43 -0400

    removed fail project

--- src/me/dancrawley/project/passwordfail/Label.java
+++
@@ -1,9 +0,0 @@
-package me.dancrawley.project.passwordfail;
-
-/**
- * Created by 257019 on 5/18/2015.
- */
-public class Label {
-
-
-}
--- src/me/dancrawley/project/passwordfail/Newman.java
+++
@@ -1,8 +0,0 @@
-package me.dancrawley.project.passwordfail;
-
-/**
- * Created by 257019 on 5/15/2015.
- */
-public class Newman {
-
-}
